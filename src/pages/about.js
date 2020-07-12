fd8b88d38d863c87d4f8283ecd04177d489bc45d
sra1rdy <sravanreddy235@gmail.com>
2020-07-12 17:43:26 +0200

    added aboutme page content + bug fix query in gallery

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import Helmet from "react-helmet"
 import Layout from "../components/layout"
 import { graphql } from 'gatsby'
-import aboutImg from "../images/default.jpg"
+import aboutImg from "../images/Lachi-original.jpeg"
 
 export class About extends Component {
   render() {
@@ -23,10 +23,10 @@ export class About extends Component {
                     <div className="col-lg-8">
                         <h4>About Me</h4>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                          I'm Laxmi Narayana Kotari, an Engineer and an Artist. I transform digital images into artistic views. 
                         </p>
                         <p>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32
+                          If you like to have a hand drawn pictures of your family, pets or any image that you want to get drawn into art. Please don't hesitate to contact me.
                         </p>
                     </div>
              
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { graphql } from 'gatsby'
 import classNames from "classnames";
 
 
